809a8ea61bff9f72829c0d24f3e9ca30568177ac
hugosenari <hugosenari@gmail.com>
2022-03-12 04:15:49 -0300

    chore: re index nimsha2

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Secure Hash Algorithm - 2, [224, 256, 384, and 512 bits]'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimsha2-master".url = "path:./master";
+  inputs."nimsha2-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimsha2-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
