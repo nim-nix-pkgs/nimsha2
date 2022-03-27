6209a89df8d1e19ae1ac87edc88a7026f3eefe55
hugosenari <hugosenari@gmail.com>
2022-03-27 18:23:52 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-nimSHA2-master.ref   = "refs/heads/master";
   inputs.src-nimSHA2-master.owner = "jangko";
   inputs.src-nimSHA2-master.repo  = "nimSHA2";
+  inputs.src-nimSHA2-master.dir   = "";
   inputs.src-nimSHA2-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
